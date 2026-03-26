24b7cc988aee555f023ec72276ede4d97fd28a67
MarketingClinDigital <marketing@clin.digital>
2026-03-26 17:02:43 -0300

    ajustes paginas

--- js/scripts.js
+++ js/scripts.js
@@ -182,7 +182,7 @@
 		    sectionSelector: '.section',
 		    loopBottom: false,
 		    loopTop: false,
-		    anchors: ['page1', 'page1-2', 'page3', 'page4-2', 'page-blank-methodology', 'page4', 'page6', 'page-methodology'],
+		    anchors: ['home', 'estrategia', 'timeline', 'presenca', 'metodologia', 'advisor', 'solucoes', 'informacoes'],
 		    afterLoad: function(anchorLink, index){
 	           navbarFullpage();
 	            
